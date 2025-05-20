2f6a5f317bffaedf89ecc1346b7a526512ee5115
CaoZ <caoz@rock-it.org>
2025-05-20 20:41:38 +0800

    lame fix.

--- packages/lame.cmake
+++ packages/lame.cmake
@@ -1,26 +1,20 @@
-set(DEBPATCH ${CMAKE_CURRENT_BINARY_DIR}/lame-prefix/src/patch.sh)
-file(WRITE ${DEBPATCH}
-"#!/bin/bash
-for i in $(cat debian/patches/series); do
-    patch -N -p1 < debian/patches/$i || true
-done")
-
-ExternalProject_Add(lame
-    GIT_REPOSITORY https://gitlab.com/shinchiro/lame.git
-    SOURCE_DIR ${SOURCE_LOCATION}
-    GIT_CLONE_FLAGS "--filter=tree:0"
-    UPDATE_COMMAND ""
-    PATCH_COMMAND chmod 755 ${DEBPATCH} && ${DEBPATCH}
-    CONFIGURE_COMMAND ${EXEC} autoreconf -fi && CONF=1 <SOURCE_DIR>/configure
-        --host=${TARGET_ARCH}
-        --prefix=${MINGW_INSTALL_PREFIX}
-        --disable-shared
-        --disable-frontend
-    BUILD_COMMAND ${MAKE}
-    INSTALL_COMMAND ${MAKE} install
-    BUILD_IN_SOURCE 1
-    LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
-)
+ExternalProject_Add(
+  lame
+  GIT_REPOSITORY https://gitlab.com/shinchiro/lame.git
+  GIT_SHALLOW 1
+  PATCH_COMMAND ${EXEC} git am -3 ${CMAKE_CURRENT_SOURCE_DIR}/lame-*.patch
+  UPDATE_COMMAND ""
+  CONFIGURE_COMMAND
+    ${EXEC} autoupdate -f && autoreconf -fi && <SOURCE_DIR>/configure
+    ${autotools_conf_args}
+    --disable-frontend
+  BUILD_COMMAND ${MAKE}
+  INSTALL_COMMAND ${MAKE} install
+  BUILD_IN_SOURCE 1
+  LOG_DOWNLOAD 1
+  LOG_UPDATE 1
+  LOG_CONFIGURE 1
+  LOG_BUILD 1
+  LOG_INSTALL 1)
 
 force_rebuild_git(lame)
-cleanup(lame install)
